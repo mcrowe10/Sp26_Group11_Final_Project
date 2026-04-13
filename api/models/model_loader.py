7b7d2fdcc19731f87bcab161da04696ab0a4f577
Matthew Crowe <mcrowe10@charlotte.edu>
2026-04-13 14:37:19 -0400

    Updated model_loader

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources
+from . import orders, order_details, recipes, sandwiches, resources, customers, payments, promotion, reviews
 
 from ..dependencies.database import engine
 
@@ -9,3 +9,7 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
+    customers.Base.metadata.create_all(engine)
+    payments.Base.metadata.create_all(engine)
+    promotion.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
