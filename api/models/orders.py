dbf975fd42562c3ae75321ecd006dbc9a7be117f
bemaus <benas.markauskas1@icloud.com>
2026-04-27 15:24:34 -0400

    Simple update for orders

--- api/models/orders.py
+++ api/models/orders.py
@@ -7,6 +7,7 @@ from ..dependencies.database import Base
 class Order(Base):
     __tablename__ = "orders"
 
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100))
     customer_id = Column(Integer, ForeignKey("customers.id"), nullable=True)
